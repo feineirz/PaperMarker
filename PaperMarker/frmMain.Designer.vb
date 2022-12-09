b4d27c19a76980a01bb64646e7cdad655739f485
feineirz <feineirz@live.com>
2022-12-10 03:06:33 +0700

    Added: Crosshair pattern.

--- PaperMarker/frmMain.Designer.vb
+++ PaperMarker/frmMain.Designer.vb
@@ -25,24 +25,38 @@ Partial Class frmMain
 		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
 		Me.btnPrint = New System.Windows.Forms.Button()
 		Me.grpPatternType = New System.Windows.Forms.GroupBox()
+		Me.Label5 = New System.Windows.Forms.Label()
+		Me.numDotSize = New System.Windows.Forms.NumericUpDown()
+		Me.Label6 = New System.Windows.Forms.Label()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.numDotSpace = New System.Windows.Forms.NumericUpDown()
+		Me.Label4 = New System.Windows.Forms.Label()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.numLineSpace = New System.Windows.Forms.NumericUpDown()
+		Me.Label1 = New System.Windows.Forms.Label()
 		Me.rdbPatternType_Crosshair = New System.Windows.Forms.RadioButton()
 		Me.rdbPatternType_Dot = New System.Windows.Forms.RadioButton()
 		Me.rdbPatternType_Line = New System.Windows.Forms.RadioButton()
 		Me.pdcMain = New System.Drawing.Printing.PrintDocument()
 		Me.tbxStatus = New System.Windows.Forms.TextBox()
-		Me.Label1 = New System.Windows.Forms.Label()
-		Me.numLineSpace = New System.Windows.Forms.NumericUpDown()
-		Me.Label2 = New System.Windows.Forms.Label()
-		Me.Label3 = New System.Windows.Forms.Label()
-		Me.numDotSpace = New System.Windows.Forms.NumericUpDown()
-		Me.Label4 = New System.Windows.Forms.Label()
-		Me.Label5 = New System.Windows.Forms.Label()
-		Me.numDotSize = New System.Windows.Forms.NumericUpDown()
-		Me.Label6 = New System.Windows.Forms.Label()
+		Me.pnlLineSettings = New System.Windows.Forms.Panel()
+		Me.pnlDotSettings = New System.Windows.Forms.Panel()
+		Me.pnlCrosshairSettings = New System.Windows.Forms.Panel()
+		Me.numCrossSize = New System.Windows.Forms.NumericUpDown()
+		Me.Label7 = New System.Windows.Forms.Label()
+		Me.Label8 = New System.Windows.Forms.Label()
+		Me.numCrossSpace = New System.Windows.Forms.NumericUpDown()
+		Me.Label9 = New System.Windows.Forms.Label()
+		Me.Label10 = New System.Windows.Forms.Label()
 		Me.grpPatternType.SuspendLayout()
-		CType(Me.numLineSpace, System.ComponentModel.ISupportInitialize).BeginInit()
-		CType(Me.numDotSpace, System.ComponentModel.ISupportInitialize).BeginInit()
 		CType(Me.numDotSize, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.numDotSpace, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.numLineSpace, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.pnlLineSettings.SuspendLayout()
+		Me.pnlDotSettings.SuspendLayout()
+		Me.pnlCrosshairSettings.SuspendLayout()
+		CType(Me.numCrossSize, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.numCrossSpace, System.ComponentModel.ISupportInitialize).BeginInit()
 		Me.SuspendLayout()
 		'
 		'btnPrint
@@ -63,15 +77,9 @@ Partial Class frmMain
 		'grpPatternType
 		'
 		Me.grpPatternType.BackColor = System.Drawing.Color.White
-		Me.grpPatternType.Controls.Add(Me.Label5)
-		Me.grpPatternType.Controls.Add(Me.numDotSize)
-		Me.grpPatternType.Controls.Add(Me.Label6)
-		Me.grpPatternType.Controls.Add(Me.Label3)
-		Me.grpPatternType.Controls.Add(Me.numDotSpace)
-		Me.grpPatternType.Controls.Add(Me.Label4)
-		Me.grpPatternType.Controls.Add(Me.Label2)
-		Me.grpPatternType.Controls.Add(Me.numLineSpace)
-		Me.grpPatternType.Controls.Add(Me.Label1)
+		Me.grpPatternType.Controls.Add(Me.pnlCrosshairSettings)
+		Me.grpPatternType.Controls.Add(Me.pnlDotSettings)
+		Me.grpPatternType.Controls.Add(Me.pnlLineSettings)
 		Me.grpPatternType.Controls.Add(Me.rdbPatternType_Crosshair)
 		Me.grpPatternType.Controls.Add(Me.rdbPatternType_Dot)
 		Me.grpPatternType.Controls.Add(Me.rdbPatternType_Line)
@@ -85,10 +93,105 @@ Partial Class frmMain
 		Me.grpPatternType.TabStop = False
 		Me.grpPatternType.Text = "Pattern Type"
 		'
+		'Label5
+		'
+		Me.Label5.AutoSize = True
+		Me.Label5.ForeColor = System.Drawing.Color.DimGray
+		Me.Label5.Location = New System.Drawing.Point(188, 5)
+		Me.Label5.Name = "Label5"
+		Me.Label5.Size = New System.Drawing.Size(28, 15)
+		Me.Label5.TabIndex = 11
+		Me.Label5.Text = "mm."
+		'
+		'numDotSize
+		'
+		Me.numDotSize.DecimalPlaces = 1
+		Me.numDotSize.Increment = New Decimal(New Integer() {1, 0, 0, 65536})
+		Me.numDotSize.Location = New System.Drawing.Point(93, 3)
+		Me.numDotSize.Maximum = New Decimal(New Integer() {1, 0, 0, 0})
+		Me.numDotSize.Minimum = New Decimal(New Integer() {2, 0, 0, 65536})
+		Me.numDotSize.Name = "numDotSize"
+		Me.numDotSize.Size = New System.Drawing.Size(89, 23)
+		Me.numDotSize.TabIndex = 10
+		Me.numDotSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+		Me.numDotSize.Value = New Decimal(New Integer() {2, 0, 0, 65536})
+		'
+		'Label6
+		'
+		Me.Label6.AutoSize = True
+		Me.Label6.ForeColor = System.Drawing.Color.DimGray
+		Me.Label6.Location = New System.Drawing.Point(3, 5)
+		Me.Label6.Name = "Label6"
+		Me.Label6.Size = New System.Drawing.Size(63, 15)
+		Me.Label6.TabIndex = 9
+		Me.Label6.Text = "Dot size"
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.ForeColor = System.Drawing.Color.DimGray
+		Me.Label3.Location = New System.Drawing.Point(188, 34)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(28, 15)
+		Me.Label3.TabIndex = 8
+		Me.Label3.Text = "mm."
+		'
+		'numDotSpace
+		'
+		Me.numDotSpace.Location = New System.Drawing.Point(93, 32)
+		Me.numDotSpace.Maximum = New Decimal(New Integer() {10, 0, 0, 0})
+		Me.numDotSpace.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+		Me.numDotSpace.Name = "numDotSpace"
+		Me.numDotSpace.Size = New System.Drawing.Size(89, 23)
+		Me.numDotSpace.TabIndex = 7
+		Me.numDotSpace.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+		Me.numDotSpace.Value = New Decimal(New Integer() {5, 0, 0, 0})
+		'
+		'Label4
+		'
+		Me.Label4.AutoSize = True
+		Me.Label4.ForeColor = System.Drawing.Color.DimGray
+		Me.Label4.Location = New System.Drawing.Point(3, 34)
+		Me.Label4.Name = "Label4"
+		Me.Label4.Size = New System.Drawing.Size(70, 15)
+		Me.Label4.TabIndex = 6
+		Me.Label4.Text = "Dot space"
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.ForeColor = System.Drawing.Color.DimGray
+		Me.Label2.Location = New System.Drawing.Point(190, 5)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(28, 15)
+		Me.Label2.TabIndex = 5
+		Me.Label2.Text = "mm."
+		'
+		'numLineSpace
+		'
+		Me.numLineSpace.Location = New System.Drawing.Point(95, 3)
+		Me.numLineSpace.Maximum = New Decimal(New Integer() {20, 0, 0, 0})
+		Me.numLineSpace.Minimum = New Decimal(New Integer() {4, 0, 0, 0})
+		Me.numLineSpace.Name = "numLineSpace"
+		Me.numLineSpace.Size = New System.Drawing.Size(89, 23)
+		Me.numLineSpace.TabIndex = 4
+		Me.numLineSpace.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+		Me.numLineSpace.Value = New Decimal(New Integer() {7, 0, 0, 0})
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.ForeColor = System.Drawing.Color.DimGray
+		Me.Label1.Location = New System.Drawing.Point(5, 5)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(77, 15)
+		Me.Label1.TabIndex = 3
+		Me.Label1.Text = "Line space"
+		'
 		'rdbPatternType_Crosshair
 		'
 		Me.rdbPatternType_Crosshair.AutoSize = True
-		Me.rdbPatternType_Crosshair.Location = New System.Drawing.Point(27, 153)
+		Me.rdbPatternType_Crosshair.Location = New System.Drawing.Point(27, 176)
 		Me.rdbPatternType_Crosshair.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
 		Me.rdbPatternType_Crosshair.Name = "rdbPatternType_Crosshair"
 		Me.rdbPatternType_Crosshair.Size = New System.Drawing.Size(88, 19)
@@ -99,7 +202,7 @@ Partial Class frmMain
 		'rdbPatternType_Dot
 		'
 		Me.rdbPatternType_Dot.AutoSize = True
-		Me.rdbPatternType_Dot.Location = New System.Drawing.Point(27, 74)
+		Me.rdbPatternType_Dot.Location = New System.Drawing.Point(27, 85)
 		Me.rdbPatternType_Dot.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
 		Me.rdbPatternType_Dot.Name = "rdbPatternType_Dot"
 		Me.rdbPatternType_Dot.Size = New System.Drawing.Size(46, 19)
@@ -135,100 +238,105 @@ Partial Class frmMain
 		Me.tbxStatus.Size = New System.Drawing.Size(490, 270)
 		Me.tbxStatus.TabIndex = 4
 		'
-		'Label1
+		'pnlLineSettings
 		'
-		Me.Label1.AutoSize = True
-		Me.Label1.ForeColor = System.Drawing.Color.DimGray
-		Me.Label1.Location = New System.Drawing.Point(53, 47)
-		Me.Label1.Name = "Label1"
-		Me.Label1.Size = New System.Drawing.Size(77, 15)
-		Me.Label1.TabIndex = 3
-		Me.Label1.Text = "Line space"
+		Me.pnlLineSettings.Controls.Add(Me.Label1)
+		Me.pnlLineSettings.Controls.Add(Me.numLineSpace)
+		Me.pnlLineSettings.Controls.Add(Me.Label2)
+		Me.pnlLineSettings.Location = New System.Drawing.Point(47, 50)
+		Me.pnlLineSettings.Name = "pnlLineSettings"
+		Me.pnlLineSettings.Size = New System.Drawing.Size(220, 30)
+		Me.pnlLineSettings.TabIndex = 5
 		'
-		'numLineSpace
+		'pnlDotSettings
 		'
-		Me.numLineSpace.Location = New System.Drawing.Point(143, 45)
-		Me.numLineSpace.Maximum = New Decimal(New Integer() {20, 0, 0, 0})
-		Me.numLineSpace.Minimum = New Decimal(New Integer() {4, 0, 0, 0})
-		Me.numLineSpace.Name = "numLineSpace"
-		Me.numLineSpace.Size = New System.Drawing.Size(89, 23)
-		Me.numLineSpace.TabIndex = 4
-		Me.numLineSpace.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
-		Me.numLineSpace.Value = New Decimal(New Integer() {7, 0, 0, 0})
+		Me.pnlDotSettings.Controls.Add(Me.numDotSize)
+		Me.pnlDotSettings.Controls.Add(Me.Label5)
+		Me.pnlDotSettings.Controls.Add(Me.Label4)
+		Me.pnlDotSettings.Controls.Add(Me.numDotSpace)
+		Me.pnlDotSettings.Controls.Add(Me.Label6)
+		Me.pnlDotSettings.Controls.Add(Me.Label3)
+		Me.pnlDotSettings.Location = New System.Drawing.Point(47, 110)
+		Me.pnlDotSettings.Name = "pnlDotSettings"
+		Me.pnlDotSettings.Size = New System.Drawing.Size(220, 60)
+		Me.pnlDotSettings.TabIndex = 5
 		'
-		'Label2
+		'pnlCrosshairSettings
 		'
-		Me.Label2.AutoSize = True
-		Me.Label2.ForeColor = System.Drawing.Color.DimGray
-		Me.Label2.Location = New System.Drawing.Point(238, 47)
-		Me.Label2.Name = "Label2"
-		Me.Label2.Size = New System.Drawing.Size(28, 15)
-		Me.Label2.TabIndex = 5
-		Me.Label2.Text = "mm."
+		Me.pnlCrosshairSettings.Controls.Add(Me.numCrossSize)
+		Me.pnlCrosshairSettings.Controls.Add(Me.Label7)
+		Me.pnlCrosshairSettings.Controls.Add(Me.Label8)
+		Me.pnlCrosshairSettings.Controls.Add(Me.numCrossSpace)
+		Me.pnlCrosshairSettings.Controls.Add(Me.Label9)
+		Me.pnlCrosshairSettings.Controls.Add(Me.Label10)
+		Me.pnlCrosshairSettings.Location = New System.Drawing.Point(47, 201)
+		Me.pnlCrosshairSettings.Name = "pnlCrosshairSettings"
+		Me.pnlCrosshairSettings.Size = New System.Drawing.Size(220, 60)
+		Me.pnlCrosshairSettings.TabIndex = 12
 		'
-		'Label3
+		'numCrossSize
 		'
-		Me.Label3.AutoSize = True
-		Me.Label3.ForeColor = System.Drawing.Color.DimGray
-		Me.Label3.Location = New System.Drawing.Point(238, 125)
-		Me.Label3.Name = "Label3"
-		Me.Label3.Size = New System.Drawing.Size(28, 15)
-		Me.Label3.TabIndex = 8
-		Me.Label3.Text = "mm."
+		Me.numCrossSize.DecimalPlaces = 1
+		Me.numCrossSize.Increment = New Decimal(New Integer() {2, 0, 0, 65536})
+		Me.numCrossSize.Location = New System.Drawing.Point(93, 3)
+		Me.numCrossSize.Maximum = New Decimal(New Integer() {25, 0, 0, 65536})
+		Me.numCrossSize.Minimum = New Decimal(New Integer() {4, 0, 0, 65536})
+		Me.numCrossSize.Name = "numCrossSize"
+		Me.numCrossSize.Size = New System.Drawing.Size(89, 23)
+		Me.numCrossSize.TabIndex = 10
+		Me.numCrossSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+		Me.numCrossSize.Value = New Decimal(New Integer() {4, 0, 0, 65536})
 		'
-		'numDotSpace
+		'Label7
 		'
-		Me.numDotSpace.Location = New System.Drawing.Point(143, 123)
-		Me.numDotSpace.Maximum = New Decimal(New Integer() {10, 0, 0, 0})
-		Me.numDotSpace.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
-		Me.numDotSpace.Name = "numDotSpace"
-		Me.numDotSpace.Size = New System.Drawing.Size(89, 23)
-		Me.numDotSpace.TabIndex = 7
-		Me.numDotSpace.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
-		Me.numDotSpace.Value = New Decimal(New Integer() {5, 0, 0, 0})
+		Me.Label7.AutoSize = True
+		Me.Label7.ForeColor = System.Drawing.Color.DimGray
+		Me.Label7.Location = New System.Drawing.Point(188, 5)
+		Me.Label7.Name = "Label7"
+		Me.Label7.Size = New System.Drawing.Size(28, 15)
+		Me.Label7.TabIndex = 11
+		Me.Label7.Text = "mm."
 		'
-		'Label4
+		'Label8
 		'
-		Me.Label4.AutoSize = True
-		Me.Label4.ForeColor = System.Drawing.Color.DimGray
-		Me.Label4.Location = New System.Drawing.Point(53, 125)
-		Me.Label4.Name = "Label4"
-		Me.Label4.Size = New System.Drawing.Size(70, 15)
-		Me.Label4.TabIndex = 6
-		Me.Label4.Text = "Dot space"
+		Me.Label8.AutoSize = True
+		Me.Label8.ForeColor = System.Drawing.Color.DimGray
+		Me.Label8.Location = New System.Drawing.Point(3, 34)
+		Me.Label8.Name = "Label8"
+		Me.Label8.Size = New System.Drawing.Size(77, 15)
+		Me.Label8.TabIndex = 6
+		Me.Label8.Text = "Mark space"
 		'
-		'Label5
+		'numCrossSpace
 		'
-		Me.Label5.AutoSize = True
-		Me.Label5.ForeColor = System.Drawing.Color.DimGray
-		Me.Label5.Location = New System.Drawing.Point(238, 96)
-		Me.Label5.Name = "Label5"
-		Me.Label5.Size = New System.Drawing.Size(28, 15)
-		Me.Label5.TabIndex = 11
-		Me.Label5.Text = "mm."
+		Me.numCrossSpace.Location = New System.Drawing.Point(93, 32)
+		Me.numCrossSpace.Maximum = New Decimal(New Integer() {10, 0, 0, 0})
+		Me.numCrossSpace.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+		Me.numCrossSpace.Name = "numCrossSpace"
+		Me.numCrossSpace.Size = New System.Drawing.Size(89, 23)
+		Me.numCrossSpace.TabIndex = 7
+		Me.numCrossSpace.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+		Me.numCrossSpace.Value = New Decimal(New Integer() {5, 0, 0, 0})
 		'
-		'numDotSize
+		'Label9
 		'
-		Me.numDotSize.DecimalPlaces = 1
-		Me.numDotSize.Increment = New Decimal(New Integer() {1, 0, 0, 65536})
-		Me.numDotSize.Location = New System.Drawing.Point(143, 94)
-		Me.numDotSize.Maximum = New Decimal(New Integer() {1, 0, 0, 0})
-		Me.numDotSize.Minimum = New Decimal(New Integer() {2, 0, 0, 65536})
-		Me.numDotSize.Name = "numDotSize"
-		Me.numDotSize.Size = New System.Drawing.Size(89, 23)
-		Me.numDotSize.TabIndex = 10
-		Me.numDotSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
-		Me.numDotSize.Value = New Decimal(New Integer() {3, 0, 0, 65536})
+		Me.Label9.AutoSize = True
+		Me.Label9.ForeColor = System.Drawing.Color.DimGray
+		Me.Label9.Location = New System.Drawing.Point(3, 5)
+		Me.Label9.Name = "Label9"
+		Me.Label9.Size = New System.Drawing.Size(70, 15)
+		Me.Label9.TabIndex = 9
+		Me.Label9.Text = "Mark size"
 		'
-		'Label6
+		'Label10
 		'
-		Me.Label6.AutoSize = True
-		Me.Label6.ForeColor = System.Drawing.Color.DimGray
-		Me.Label6.Location = New System.Drawing.Point(53, 96)
-		Me.Label6.Name = "Label6"
-		Me.Label6.Size = New System.Drawing.Size(63, 15)
-		Me.Label6.TabIndex = 9
-		Me.Label6.Text = "Dot size"
+		Me.Label10.AutoSize = True
+		Me.Label10.ForeColor = System.Drawing.Color.DimGray
+		Me.Label10.Location = New System.Drawing.Point(188, 34)
+		Me.Label10.Name = "Label10"
+		Me.Label10.Size = New System.Drawing.Size(28, 15)
+		Me.Label10.TabIndex = 8
+		Me.Label10.Text = "mm."
 		'
 		'frmMain
 		'
@@ -249,9 +357,17 @@ Partial Class frmMain
 		Me.Text = "Paper Marker"
 		Me.grpPatternType.ResumeLayout(False)
 		Me.grpPatternType.PerformLayout()
-		CType(Me.numLineSpace, System.ComponentModel.ISupportInitialize).EndInit()
-		CType(Me.numDotSpace, System.ComponentModel.ISupportInitialize).EndInit()
 		CType(Me.numDotSize, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.numDotSpace, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.numLineSpace, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.pnlLineSettings.ResumeLayout(False)
+		Me.pnlLineSettings.PerformLayout()
+		Me.pnlDotSettings.ResumeLayout(False)
+		Me.pnlDotSettings.PerformLayout()
+		Me.pnlCrosshairSettings.ResumeLayout(False)
+		Me.pnlCrosshairSettings.PerformLayout()
+		CType(Me.numCrossSize, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.numCrossSpace, System.ComponentModel.ISupportInitialize).EndInit()
 		Me.ResumeLayout(False)
 		Me.PerformLayout()
 
@@ -273,4 +389,13 @@ Partial Class frmMain
 	Friend WithEvents Label3 As Label
 	Friend WithEvents numDotSpace As NumericUpDown
 	Friend WithEvents Label4 As Label
+	Friend WithEvents pnlDotSettings As Panel
+	Friend WithEvents pnlLineSettings As Panel
+	Friend WithEvents pnlCrosshairSettings As Panel
+	Friend WithEvents numCrossSize As NumericUpDown
+	Friend WithEvents Label7 As Label
+	Friend WithEvents Label8 As Label
+	Friend WithEvents numCrossSpace As NumericUpDown
+	Friend WithEvents Label9 As Label
+	Friend WithEvents Label10 As Label
 End Class
--- PaperMarker/frmMain.vb
+++ PaperMarker/frmMain.vb
@@ -10,8 +10,8 @@
         g.TextRenderingHint = Drawing.Text.TextRenderingHint.AntiAlias
         g.SmoothingMode = Drawing2D.SmoothingMode.AntiAlias
 
-        Dim defaultPen As New Pen(Color.FromArgb(255, 150, 150, 150), 0.2)
-        Dim defaultBrush As New SolidBrush(Color.FromArgb(255, 150, 150, 150))
+        Dim defaultPen As New Pen(Color.FromArgb(255, 200, 200, 200), 0.2)
+        Dim defaultBrush As New SolidBrush(Color.FromArgb(255, 200, 200, 200))
 
         Dim paperSize As New Size(pdcMain.DefaultPageSettings.PaperSize.Width, pdcMain.DefaultPageSettings.PaperSize.Height) 'pixel
         Dim paperPadding As New Size(10 * factorMillimeterToPixel, 10 * factorMillimeterToPixel) 'pixel
@@ -24,7 +24,10 @@
             Dim lineAdjustSpace As Integer = (workingArea.Height - (lineCount * lineSpace)) / 2
 
             For i = 0 To lineCount
-                g.DrawLine(defaultPen, New Point(initPoint.X, initPoint.Y + lineAdjustSpace + (i * lineSpace)), New Point(initPoint.X + workingArea.Width, initPoint.Y + lineAdjustSpace + (i * lineSpace)))
+                g.DrawLine(
+                    defaultPen,
+                    New Point(initPoint.X, initPoint.Y + lineAdjustSpace + (i * lineSpace)),
+                    New Point(initPoint.X + workingArea.Width, initPoint.Y + lineAdjustSpace + (i * lineSpace)))
             Next
 
         ElseIf patternType = "Dot" Then
@@ -39,9 +42,38 @@
 
             For i = 0 To dotCount.Width
                 For j = 0 To dotCount.Height
-                    g.FillEllipse(defaultBrush, initPoint.X + dotAdjustSpace.Width + (i * dotSpace), initPoint.Y + dotAdjustSpace.Height + (j * dotSpace), dotSize, dotSize)
+                    g.FillEllipse(
+                        defaultBrush,
+                        initPoint.X + dotAdjustSpace.Width + (i * dotSpace),
+                        initPoint.Y + dotAdjustSpace.Height + (j * dotSpace),
+                        dotSize,
+                        dotSize)
                 Next
             Next
+
+        ElseIf patternType = "Crosshair" Then
+            Dim crossSize As Integer = numCrossSize.Value * factorMillimeterToPixel
+            Dim crossSpace As Integer = numCrossSpace.Value * factorMillimeterToPixel
+            Dim crossCount As New Size(
+                Math.Floor(workingArea.Width / crossSpace),
+                Math.Floor(workingArea.Height / crossSpace))
+            Dim crossAdjustSpace As New Size(
+                (workingArea.Width - (crossCount.Width * crossSpace)) / 2,
+                (workingArea.Height - (crossCount.Height * crossSpace)) / 2)
+
+            For i = 0 To crossCount.Width
+                For j = 0 To crossCount.Height
+                    g.DrawLine(
+                        defaultPen,
+                        New Point(initPoint.X + crossAdjustSpace.Width + (i * crossSpace), initPoint.Y + crossAdjustSpace.Height + (0.5 * crossSize) + (j * crossSpace)),
+                        New Point(initPoint.X + crossAdjustSpace.Width + crossSize + (i * crossSpace), initPoint.Y + crossAdjustSpace.Height + (0.5 * crossSize) + (j * crossSpace)))
+                    g.DrawLine(
+                        defaultPen,
+                        New Point(initPoint.X + crossAdjustSpace.Width + (0.5 * crossSize) + (i * crossSpace), initPoint.Y + crossAdjustSpace.Height + (j * crossSpace)),
+                        New Point(initPoint.X + crossAdjustSpace.Width + (0.5 * crossSize) + (i * crossSpace), initPoint.Y + crossAdjustSpace.Height + crossSize + (j * crossSpace)))
+                Next
+            Next
+
         End If
 
         e.HasMorePages = False
@@ -62,7 +94,7 @@
                 pdcMain.Print()
                 tbxStatus.Text = "Printing successful." & vbCrLf & tbxStatus.Text
             Catch ex As Exception
-                tbxStatus.Text = "Printing fail! (Output file might be in use)." & vbCrLf & tbxStatus.Text
+                tbxStatus.Text = "Printing fail! (Output file might be in use.)" & vbCrLf & tbxStatus.Text
             End Try
         End If
 
@@ -70,18 +102,42 @@
 
     Private Sub rdbPatternType_CheckedChanged(sender As Object, e As EventArgs) Handles rdbPatternType_Line.CheckedChanged, rdbPatternType_Dot.CheckedChanged, rdbPatternType_Crosshair.CheckedChanged
         Dim rdb As RadioButton = DirectCast(sender, RadioButton)
+
+        pnlLineSettings.Enabled = False
+        pnlDotSettings.Enabled = False
+        pnlCrosshairSettings.Enabled = False
+
         If rdb.Checked Then
             Select Case rdb.Text
                 Case ""
                     patternType = "Line"
                     tbxStatus.Text = "Pattern Type: Line [Line space 7 mm.]" & vbCrLf & tbxStatus.Text
+                    pnlLineSettings.Enabled = True
                 Case "Line"
                     patternType = "Line"
                     tbxStatus.Text = "Pattern Type: Line [Line space " & numLineSpace.Value & " mm.]" & vbCrLf & tbxStatus.Text
+                    pnlLineSettings.Enabled = True
                 Case "Dot"
                     patternType = "Dot"
                     tbxStatus.Text = "Pattern Type: Dot [Dot size " & numDotSize.Value & " mm., Dot space " & numDotSpace.Value & " mm.]" & vbCrLf & tbxStatus.Text
+                    pnlDotSettings.Enabled = True
+                Case "Crosshair"
+                    patternType = "Crosshair"
+                    tbxStatus.Text = "Pattern Type: Crosshair [Mark size " & numCrossSize.Value & " mm., Mark space " & numCrossSpace.Value & " mm.]" & vbCrLf & tbxStatus.Text
+                    pnlCrosshairSettings.Enabled = True
             End Select
         End If
+    End Sub
+
+    Private Sub numLineSpace_ValueChanged(sender As Object, e As EventArgs) Handles numLineSpace.ValueChanged
+        If rdbPatternType_Line.Checked Then tbxStatus.Text = "Pattern Type: Line [Line space " & numLineSpace.Value & " mm.]" & vbCrLf & tbxStatus.Text
+    End Sub
+
+    Private Sub numDot_ValueChanged(sender As Object, e As EventArgs) Handles numDotSize.ValueChanged, numDotSpace.ValueChanged
+        If rdbPatternType_Dot.Checked Then tbxStatus.Text = "Pattern Type: Dot [Dot size " & numDotSize.Value & " mm., Dot space " & numDotSpace.Value & " mm.]" & vbCrLf & tbxStatus.Text
+    End Sub
+
+    Private Sub numCross_ValueChanged(sender As Object, e As EventArgs) Handles numCrossSize.ValueChanged, numCrossSpace.ValueChanged
+        If rdbPatternType_Crosshair.Checked Then tbxStatus.Text = "Pattern Type: Crosshair [Mark size " & numCrossSize.Value & " mm., Mark space " & numCrossSpace.Value & " mm.]" & vbCrLf & tbxStatus.Text
     End Sub
 End Class
